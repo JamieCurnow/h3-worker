060f3cf8a404b01ce3af4844a2c732fad84b567f
JamieCurnow <jamie@island-web.ca>
2021-10-14 12:37:03 +0100

    initial commit - getting some ts errors

---
+++ README.md
@@ -0,0 +1,2 @@
+# h3-worker
+Use h3 in a (Cloudflare) worker
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { App, useBase } from 'h3'
+import { createCall } from 'unenv/runtime/fetch/index'
+import { Handle } from 'unenv/runtime/fetch/call'
+
+export interface WorkerWrapperOptions {
+  // The base path for the app - defaults to '/'
+  basePath?: string
+}
+
+export const handleEvent = async (event: FetchEvent, app: App, opts?: WorkerWrapperOptions): Promise<Response> => {
+  const handle = useBase(opts?.basePath || '/', app) as unknown as Handle
+  const localCall = createCall(handle)
+  const url = new URL(event.request.url)
+
+  const clonedHeaders: { [key: string]: string | string[] } = {}
+  event.request.headers.forEach((v, k) => clonedHeaders[k] = v)
+
+  const r = await localCall({
+    event,
+    url: url.toString(),
+    host: url.hostname,
+    protocol: url.protocol,
+    headers: clonedHeaders,
+    method: event.request.method,
+    redirect: event.request.redirect,
+    body: event.request.body
+  })
+
+  return new Response(r.body, {
+    headers: r.headers as HeadersInit,
+    status: r.status,
+    statusText: r.statusText
+  })
+}
